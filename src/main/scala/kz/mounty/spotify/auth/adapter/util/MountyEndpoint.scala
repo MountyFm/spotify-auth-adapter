b745597c6f3be2528f42eb0969ef658ff8b069a9
dmoldabayev <dmoldabayev.99@gmail.com>
2021-11-26 01:09:26 +0600

    changed errorSeries name

--- src/main/scala/kz/mounty/spotify/auth/adapter/util/MountyEndpoint.scala
+++ src/main/scala/kz/mounty/spotify/auth/adapter/util/MountyEndpoint.scala
@@ -3,5 +3,5 @@ package kz.mounty.spotify.auth.adapter.util
 import kz.mounty.fm.exceptions.ErrorSeries
 
 trait MountyEndpoint {
-  implicit val errorSeries: ErrorSeries = ErrorSeries.AUTH_API
+  implicit val errorSeries: ErrorSeries = ErrorSeries.SPOTIFY_AUTH_ADAPTER
 }
